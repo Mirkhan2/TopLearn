b377d6b9cded0543eaac182752e2914760c1c772
Mirkhan2 <mirkhan.shams2@gmail.com>
2023-10-02 23:39:33 +0200

    Add project files.

---
+++ TopLearn.DataLayer/Entities/User/User.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopLearn.DataLayer.Entities.User
+{
+	public class User
+	{
+
+        public User()
+        {
+            
+        }
+        [Key]
+        public int UserId { get; set; }
+
+        [Display(Name = "UserName")]
+        [Required(ErrorMessage ="Please Add{0}")]
+        [MaxLength(100 , ErrorMessage ="Its Cant be Longer{1} than{0}")]
+        public string UserName { get; set; }
+
+		[Display(Name = "Email")]
+		[Required(ErrorMessage = "Please Add{0}")]
+		[MaxLength(100, ErrorMessage = "Its Cant be Longer{1} than{0}")]
+        [EmailAddress(ErrorMessage ="Email doesnt Exit")]
+		public string Email { get; set; }
+
+
+		[Display(Name = "Password")]
+		[Required(ErrorMessage = "Please Add{0}")]
+		[MaxLength(100, ErrorMessage = "Its Cant be Longer{1} than{0}")]
+		public string  Password { get; set; }
+
+		[Display(Name = "ActiveCode")]
+		protected string ActiveCode {  get; set; }
+
+		[Display(Name = "Activr")]
+		public bool IsActive { get; set; }
+
+		[Display(Name = "Avatar")]
+		public string  UserAvatar { get; set; }
+
+		[Display(Name = "Date")]
+		public DateTime RegisterDate { get; set; }
+
+		#region Relations 
+
+		public virtual  List<UserRole> UserRoles { get; set; }
+
+		#endregion
+
+	}
+}
